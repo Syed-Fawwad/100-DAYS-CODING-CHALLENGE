b6e571f38a399f8a503a0c66ba688adfa99bfe89
SYED FAWWAD <160050947+Syed-Fawwad@users.noreply.github.com>
2024-03-11 17:07:33 +0500

    Day 1/100
    day 1 of , 100 days coding challenge.

---
+++ app.ts
@@ -0,0 +1,26 @@
+/*100 days of coding challenge
+DAY 1/100 :start coding
+Question No 1
+installation of node js , typescript , VS code 
+
+Question No 2
+Question 2: Personal Message: Store a person's name in a variable and print a 
+message to them, like "Hello Asharib, would you like to learn some TypeScript today?"*/
+
+let personName1: string= "FAWWAD";//this is name
+console.log(`hello ${personName1} would you like to learn some TYPESCRIPT today?`);//this shows the message
+
+/*Question No 3
+Question 3: Name Cases: Store a person's name in a variable,
+ and then print that person's name in lowercase, uppercase, and titlecase.*/
+
+ //lower case
+let personName: string ="Fawwad";
+console.log("lowercase:", personName.toLowerCase());
+
+//upper case
+=
+console.log("uppercase:",personName.toLocaleUpperCase());
+
+//title case
+console.log("titlecase:",personName.replace(/\)bw/g,c=>c.toUpperCase()));
