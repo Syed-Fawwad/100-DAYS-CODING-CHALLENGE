d57bce739e122f6f6bf30e99230fd8829591915d
SYED FAWWAD <160050947+Syed-Fawwad@users.noreply.github.com>
2024-03-14 07:20:47 +0500

    DAY 4/100
    completing day 4 assignment of 100 days coding challenge.

--- app.ts
+++ app.ts
@@ -1,11 +1,20 @@
-/*Day 3 Challenge: Start Coding!
+/*Day 4 Challenge: Start Coding!
 
-Question 7,8: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.*/
-console.log(5 +3);//addition
-console.log(12 -4);//subtraction
-console.log(2 *4);//multiplication
-console.log(32 /4);//division
+Question 10: Adding Comments: Choose two of the programs you've written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
 
-/*Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.*/
-let favNum:number=16;
-console.log(`my favorite number is ${favNum}. `)//this shows the msg with your fav number
+Fawwad 2024-04-16
+this program print a personal message */ 
+let myName :string="FAWWAD";
+console.log(`Hello ${myName} would you like to complete your todays assignment?`);
+
+// Question 11: Names: Store the names of a few of your friends in an array called names. Print each person's name by accessing each element in the list, one at a time.
+let friendList:string[]=["SHABAN","EBAD","HUZAIFA"];
+for(let i=0;i<friendList.length;i++){
+    console.log(friendList[i]);
+}
+// Question 12: Greetings: Use the array from Exercise 11. Instead of just printing each person's name, print a message to them. The message should be the same for each person, but personalized with their name.
+let friendList1:string[]=["SHABAN","EBAD","HUZAIFA"];
+const msg:string=", would you complete your todays assignment?"
+for(let i=0;i<friendList1.length;i++){
+    console.log("Hello " +friendList1[i] +msg);
+}d
